21e7c61d03aecb8751ad74ae293698ad0f941730
Ranveer Singh PC <Ranveer Singh PC@RanveerSingh-PC.lan>
2023-04-13 17:12:43 -0400

    Find the indexOf(Object) in the code

--- src/main/java/com/mycompany/dllists_stacks_queues/DoublyLinkedLists.java
+++ src/main/java/com/mycompany/dllists_stacks_queues/DoublyLinkedLists.java
@@ -14,15 +14,22 @@ import org.w3c.dom.Node;
  * @author Ranveer Singh PC
  */
 public class DoublyLinkedLists {
-    
+    Node nodeA = newNode(0);   
+    Node nodeB = newNode(1);
     public static void main(String[] args) {
   
           DoublyLinkedList numList = new DoublyLinkedList();
-   int[] array = numList.toArray();
+   int[] array = numList.toArray();  // 1. toArray method
    System.out.print("List to array: ");
    for(int n:array){
        System.out.print("");
-   }
+   }                 // 2. find IndexOf(object)
+   int num = 0;
+   System.out.println("/nindex of "+numList.indexOf(num));
+    }
+
+    private Node newNode(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
 
