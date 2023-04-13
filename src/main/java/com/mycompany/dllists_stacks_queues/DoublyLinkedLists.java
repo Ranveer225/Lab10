af358f37d1257ef7f3801dd299b8dde7c0e0c221
Ranveer Singh PC <Ranveer Singh PC@RanveerSingh-PC.lan>
2023-04-13 17:03:48 -0400

    List and modify code to add toArray method

---
+++ src/main/java/com/mycompany/dllists_stacks_queues/DoublyLinkedLists.java
@@ -0,0 +1,27 @@
+package com.mycompany.dllists_stacks_queues;
+
+
+import com.mycompany.dllists_stacks_queues.DoublyLinkedList;
+import org.w3c.dom.Node;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ranveer Singh PC
+ */
+public class DoublyLinkedLists {
+    
+    public static void main(String[] args) {
+  
+          DoublyLinkedList numList = new DoublyLinkedList();
+   int[] array = numList.toArray();
+   System.out.print("List to array: ");
+   for(int n:array){
+       System.out.print("");
+   }
+    }
+}
